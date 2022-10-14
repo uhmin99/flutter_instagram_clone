61e7dae857f466264b4cf674aba3d85954ba0896
유현민 <yhm8622@simsimi.com>
2022-10-14 16:05:36 +0900

    chores: migrate code from flutter-wwl
    - this project was initially made in gdsc-yonsei/flutter-wwl hyunminyoo branch Week3
    - move code from flutter-wwl to here

---
+++ lib/feed_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/consts.dart';
+import 'package:flutter_instagram_clone/single_feed/single_feed.dart';
+
+class FeedList extends StatelessWidget {
+  const FeedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+            child: FeedWidget(
+              feedData: feedListData[index],
+            ),
+          );
+        },
+        itemCount: feedListData.length,
+      ),
+    );
+  }
+}
---
+++ lib/single_feed/single_feed.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/consts.dart';
+import 'package:flutter_instagram_clone/single_feed/top_title.dart';
+import 'action_buttons.dart';
+import 'comments.dart';
+import 'description.dart';
+import 'likes_text.dart';
+import 'main_image.dart';
+
+class FeedWidget extends StatelessWidget {
+  const FeedWidget({super.key, required this.feedData});
+
+  final FeedData feedData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 11,
+          ),
+          FeedTopTitle(
+              profileImgUrl: feedData.profileImgUrl,
+              name: feedData.name,
+              region: feedData.region),
+          const SizedBox(
+            height: 11,
+          ),
+          FeedImageWidget(
+            imgUrl: feedData.imgUrl,
+          ),
+          const FeedActionButtonsList(),
+          FeedLikesText(
+            likes: feedData.likes,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          FeedDescription(
+            description: feedData.description,
+            name: feedData.name,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          FeedComments(
+            comments: feedData.comments,
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
+    );
+  }
+}
